ca802093848e71da946d8e29b028fc8467d69f3d
David Chen <45245358+TheEgghead27@users.noreply.github.com>
2021-08-17 17:24:06 -0400

    Simply a difference in skill

--- remotes.sh
+++ remotes.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-git remove rename origin github # assuming cloned from github
+git remote rename origin github # assuming cloned from github
 git remote add origin https://git.suckless.org/dwm  # git pull from suckless
